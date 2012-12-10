c2694deb49bcb5608a01d6ba7f1e0890f1a4ea2d
Rigel Di Scala <rigel.discala@propylon.com>
2012-12-10 13:10:00 +0000

    solved

--- conway-spec.js
+++ conway-spec.js
@@ -14,9 +14,16 @@ describe('Conway tests', function () {
         expect(getNeighbours(univ, univ[1])[0]).toEqual(1);
     });
 
+    it('has three neighbours', function () {
+        var univ = [[0, -2], [-1, -1], [0, -1], [1, -1], [1, 0], [0, 1]];
+        expect(getNeighbours(univ, univ[0])[0]).toEqual(3);
+    });
+
     it('becomes empty', function () {
-        var univ = [[0, 1]];
-        expect(doTick(univ)).toEqual([]);
+        var univ1 = [[0, 1]];
+        expect(doTick(univ1)).toEqual([]);
+        var univ2 = [[0, 1], [1, 0], [9, 9]];
+        expect(doTick(univ2)).toEqual([]);
     });
 
     it('has one more cell and then is stable', function () {
@@ -27,10 +34,10 @@ describe('Conway tests', function () {
         expect(doTick(u2)).toEqual(u3);
     });
 
-    //it('is a glider', function () {
-    //    var u1 = [[-1, -1], [0, -1], [1, -1], [1, 0], [0, 1]];
-    //    var u2 = [];
-    //    expect(doTick(u1)).toEqual(u2);
-    //});
+    it('is a glider', function () {
+        var u1 = [[1, 0], [0, 1], [-1, -1], [0, -1], [1, -1]]
+        var u2 = [[0, -1], [0, -2], [1, 0], [1, -1], [-1, 0]]
+        expect(doTick(u1)).toEqual(u2);
+    });
 });
 
